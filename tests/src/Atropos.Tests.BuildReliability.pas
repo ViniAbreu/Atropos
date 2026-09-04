568fe9d662e74550f01da05ce1cf86b5c3c04a30
Régys Borges da Silveira <regys.silveira@gmail.com>
2026-09-04 17:17:48 -0300

    test: cover critical application orchestration paths

--- tests/src/Atropos.Tests.BuildReliability.pas
+++ tests/src/Atropos.Tests.BuildReliability.pas
@@ -13,19 +13,36 @@ type
   TProjectParserSpy = class(TInterfacedObject, IProjectParser)
   public
     ProjectUnitsCallCount: Integer;
+    Units: TArray<string>;
     function GetSearchPaths(const ADprojPath: string): TArray<string>;
     function GetProjectUnits(const ADprojPath: string): TArray<string>;
   end;
 
   TASTParserStub = class(TInterfacedObject, IASTParser)
   public
+    SyntaxTree: IUnitSyntaxTree;
+    RaiseOnParse: Boolean;
     function ParseFile(const AFilePath: string): IUnitSyntaxTree;
+  end;
+
+  TUnitSyntaxTreeStub = class(TInterfacedObject, IUnitSyntaxTree)
+  public
+    function GetUnitName: string;
+    function GetInterfaceUses: TArray<string>;
+    function GetImplementationUses: TArray<string>;
+    function GetIdentifiersUsedInInterface: TArray<string>;
+    function GetIdentifiersUsedInImplementation: TArray<string>;
+    function GetExportedIdentifiers: TArray<string>;
+    function HasInitializationSection: Boolean;
   end;
 
   TFileServiceSpy = class(TInterfacedObject, IFileService)
   public
     WriteCallCount: Integer;
     RestoreCallCount: Integer;
+    BackupCallCount: Integer;
+    CommitCallCount: Integer;
+    Content: string;
     procedure BackupFile(const AFilePath: string);
     procedure RestoreBackups;
     procedure CommitBackups;
@@ -35,6 +52,9 @@ type
 
   TReportGeneratorStub = class(TInterfacedObject, IReportGenerator)
   public
+    AddUnitCallCount: Integer;
+    AddMetricsCallCount: Integer;
+    SetInfoCallCount: Integer;
     procedure AddUnitProcessed(const AUnitName: string; const ARemovedUses, AMovedUses: TArray<string>);
     procedure AddMetrics(const ABefore, AAfter: TBuildMetrics);
     procedure SetAnalysisInfo(const AProjectName: string; AAnalysisTimeMs: Int64; AUnitsAnalyzed, ASearchPaths: Integer);
@@ -49,6 +69,7 @@ type
 
   TExternalResolverStub = class(TInterfacedObject, IExternalUnitResolver)
   public
+    ResolveKnownUnits: Boolean;
     procedure Initialize(const ASearchPaths: TArray<string>; const ADelphiPath, ABasePath: string); virtual;
     function TryResolveUnit(const AUnitName: string; out AExports: TArray<string>; out AHasInit, AIsNative: Boolean): Boolean;
   end;
@@ -67,6 +88,13 @@ type
   TSuccessfulBuildService = class(TInterfacedObject, IBuildService)
   public
     CallCount: Integer;
+    function BuildProject(const AProjectPath: string): TBuildMetrics;
+  end;
+
+  TSequencedBuildService = class(TInterfacedObject, IBuildService)
+  public
+    CallCount: Integer;
+    Results: TArray<TBuildMetrics>;
     function BuildProject(const AProjectPath: string): TBuildMetrics;
   end;
 
@@ -89,12 +117,20 @@ type
     procedure MissingDelphiEnvironmentFailsBuildGracefully;
     [Test]
     procedure HealthyProjectWithoutUnitsCommitsWithoutFinalBuild;
+    [Test]
+    procedure FailedFinalBuildRestoresModifiedFiles;
+    [Test]
+    procedure SuccessfulModificationCommitsAndRecordsMetrics;
+    [Test]
+    procedure ParserFailureSkipsUnitAndReportsProgress;
+    [Test]
+    procedure InlineHintIsFixedAndBuildIsVerifiedAgain;
   end;
 
 implementation
 
 uses
-  System.SysUtils;
+  System.SysUtils, System.IOUtils;
 
 function TProjectParserSpy.GetSearchPaths(const ADprojPath: string): TArray<string>;
 begin
@@ -104,16 +140,36 @@ end;
 function TProjectParserSpy.GetProjectUnits(const ADprojPath: string): TArray<string>;
 begin
   Inc(ProjectUnitsCallCount);
-  Result := [];
+  Result := Units;
 end;
 
 function TASTParserStub.ParseFile(const AFilePath: string): IUnitSyntaxTree;
 begin
+  if RaiseOnParse then
+    raise Exception.Create('Parser failure');
+  if Assigned(SyntaxTree) then
+    Exit(SyntaxTree);
   raise Exception.Create('AST parser must not be called after a failed baseline build.');
 end;
 
+function TUnitSyntaxTreeStub.GetUnitName: string;
+begin Result := 'TestUnit'; end;
+function TUnitSyntaxTreeStub.GetInterfaceUses: TArray<string>;
+begin Result := ['Unused.Unit']; end;
+function TUnitSyntaxTreeStub.GetImplementationUses: TArray<string>;
+begin Result := []; end;
+function TUnitSyntaxTreeStub.GetIdentifiersUsedInInterface: TArray<string>;
+begin Result := []; end;
+function TUnitSyntaxTreeStub.GetIdentifiersUsedInImplementation: TArray<string>;
+begin Result := []; end;
+function TUnitSyntaxTreeStub.GetExportedIdentifiers: TArray<string>;
+begin Result := []; end;
+function TUnitSyntaxTreeStub.HasInitializationSection: Boolean;
+begin Result := False; end;
+
 procedure TFileServiceSpy.BackupFile(const AFilePath: string);
 begin
+  Inc(BackupCallCount);
 end;
 
 procedure TFileServiceSpy.RestoreBackups;
@@ -123,28 +179,33 @@ end;
 
 procedure TFileServiceSpy.CommitBackups;
 begin
+  Inc(CommitCallCount);
 end;
 
 function TFileServiceSpy.ReadFileContent(const AFilePath: string): string;
 begin
-  Result := EmptyStr;
+  Result := Content;
 end;
 
 procedure TFileServiceSpy.WriteFileContent(const AFilePath, AContent: string);
 begin
   Inc(WriteCallCount);
+  Content := AContent;
 end;
 
 procedure TReportGeneratorStub.AddUnitProcessed(const AUnitName: string; const ARemovedUses, AMovedUses: TArray<string>);
 begin
+  Inc(AddUnitCallCount);
 end;
 
 procedure TReportGeneratorStub.AddMetrics(const ABefore, AAfter: TBuildMetrics);
 begin
+  Inc(AddMetricsCallCount);
 end;
 
 procedure TReportGeneratorStub.SetAnalysisInfo(const AProjectName: string; AAnalysisTimeMs: Int64; AUnitsAnalyzed, ASearchPaths: Integer);
 begin
+  Inc(SetInfoCallCount);
 end;
 
 function TReportGeneratorStub.GetReportContentTXT: string;
@@ -171,7 +232,9 @@ begin
   AExports := [];
   AHasInit := False;
   AIsNative := False;
-  Result := False;
+  if ResolveKnownUnits then
+    AExports := ['UnusedSymbol'];
+  Result := ResolveKnownUnits;
 end;
 
 procedure TFailingExternalResolver.Initialize(const ASearchPaths: TArray<string>; const ADelphiPath, ABasePath: string);
@@ -192,6 +255,12 @@ begin
   Inc(CallCount);
   Result := Default(TBuildMetrics);
   Result.Success := True;
+end;
+
+function TSequencedBuildService.BuildProject(const AProjectPath: string): TBuildMetrics;
+begin
+  Result := Results[CallCount];
+  Inc(CallCount);
 end;
 
 procedure TBuildReliabilityTests.NonZeroExitCodeFailsBuildWithoutErrorText;
@@ -334,6 +403,205 @@ begin
     Assert.AreEqual(0, LFileService.WriteCallCount);
   finally
     LApplicationService.Free;
+  end;
+end;
+
+procedure TBuildReliabilityTests.FailedFinalBuildRestoresModifiedFiles;
+var
+  LParser: TProjectParserSpy;
+  LAST: TASTParserStub;
+  LFiles: TFileServiceSpy;
+  LReports: TReportGeneratorStub;
+  LResolver: TExternalResolverStub;
+  LBuild: TSequencedBuildService;
+  LService: TProjectCleanerAppService;
+  LConfig: TToolConfig;
+  LTempFile: string;
+  LSuccess, LFailure: TBuildMetrics;
+begin
+  LTempFile := TPath.GetTempFileName;
+  try
+    LParser := TProjectParserSpy.Create;
+    LParser.Units := [LTempFile];
+    LAST := TASTParserStub.Create;
+    LAST.SyntaxTree := TUnitSyntaxTreeStub.Create;
+    LFiles := TFileServiceSpy.Create;
+    LFiles.Content := 'unit TestUnit;' + sLineBreak + 'interface' + sLineBreak +
+      'uses Unused.Unit;' + sLineBreak + 'implementation' + sLineBreak + 'end.';
+    LReports := TReportGeneratorStub.Create;
+    LResolver := TExternalResolverStub.Create;
+    LResolver.ResolveKnownUnits := True;
+    LSuccess := Default(TBuildMetrics);
+    LSuccess.Success := True;
+    LFailure := Default(TBuildMetrics);
+    LFailure.Success := False;
+    LFailure.ErrorMessage := 'Final build failed';
+    LBuild := TSequencedBuildService.Create;
+    LBuild.Results := [LSuccess, LFailure];
+    LConfig := TToolConfig.Default;
+    LConfig.RemoveUnused := True;
+    LService := TProjectCleanerAppService.Create(LParser, LAST, LFiles, LReports,
+      TDelphiEnvironmentStub.Create, LResolver, LBuild, LConfig);
+    try
+      LService.Execute('Project.dproj');
+      Assert.AreEqual(2, LBuild.CallCount);
+      Assert.AreEqual(1, LFiles.WriteCallCount);
+      Assert.AreEqual(1, LFiles.RestoreCallCount);
+      Assert.AreEqual(0, LFiles.CommitCallCount);
+      Assert.AreEqual(1, LReports.AddUnitCallCount);
+    finally
+      LService.Free;
+    end;
+  finally
+    TFile.Delete(LTempFile);
+  end;
+end;
+
+procedure TBuildReliabilityTests.SuccessfulModificationCommitsAndRecordsMetrics;
+var
+  LParser: TProjectParserSpy;
+  LAST: TASTParserStub;
+  LFiles: TFileServiceSpy;
+  LReports: TReportGeneratorStub;
+  LResolver: TExternalResolverStub;
+  LBuild: TSequencedBuildService;
+  LService: TProjectCleanerAppService;
+  LConfig: TToolConfig;
+  LTempFile: string;
+  LSuccess: TBuildMetrics;
+begin
+  LTempFile := TPath.GetTempFileName;
+  try
+    LParser := TProjectParserSpy.Create;
+    LParser.Units := [LTempFile];
+    LAST := TASTParserStub.Create;
+    LAST.SyntaxTree := TUnitSyntaxTreeStub.Create;
+    LFiles := TFileServiceSpy.Create;
+    LFiles.Content := 'unit TestUnit;' + sLineBreak + 'interface' + sLineBreak +
+      'uses Unused.Unit;' + sLineBreak + 'implementation' + sLineBreak + 'end.';
+    LReports := TReportGeneratorStub.Create;
+    LResolver := TExternalResolverStub.Create;
+    LResolver.ResolveKnownUnits := True;
+    LSuccess := Default(TBuildMetrics);
+    LSuccess.Success := True;
+    LBuild := TSequencedBuildService.Create;
+    LBuild.Results := [LSuccess, LSuccess];
+    LConfig := TToolConfig.Default;
+    LConfig.RemoveUnused := True;
+    LConfig.EnableDebug := True;
+    LService := TProjectCleanerAppService.Create(LParser, LAST, LFiles, LReports,
+      TDelphiEnvironmentStub.Create, LResolver, LBuild, LConfig);
+    try
+      LService.Execute('Project.dproj');
+      Assert.AreEqual(2, LBuild.CallCount);
+      Assert.AreEqual(1, LFiles.CommitCallCount);
+      Assert.AreEqual(0, LFiles.RestoreCallCount);
+      Assert.AreEqual(1, LReports.AddMetricsCallCount);
+      Assert.AreEqual(1, LReports.SetInfoCallCount);
+    finally
+      LService.Free;
+    end;
+  finally
+    TFile.Delete(LTempFile);
+  end;
+end;
+
+procedure TBuildReliabilityTests.ParserFailureSkipsUnitAndReportsProgress;
+var
+  LParser: TProjectParserSpy;
+  LAST: TASTParserStub;
+  LFiles: TFileServiceSpy;
+  LBuild: TSuccessfulBuildService;
+  LService: TProjectCleanerAppService;
+  LConfig: TToolConfig;
+  LTempFile: string;
+  LProgressPosition: Integer;
+begin
+  LTempFile := TPath.GetTempFileName;
+  try
+    LParser := TProjectParserSpy.Create;
+    LParser.Units := [LTempFile];
+    LAST := TASTParserStub.Create;
+    LAST.RaiseOnParse := True;
+    LFiles := TFileServiceSpy.Create;
+    LBuild := TSuccessfulBuildService.Create;
+    LConfig := TToolConfig.Default;
+    LService := TProjectCleanerAppService.Create(LParser, LAST, LFiles,
+      TReportGeneratorStub.Create, TDelphiEnvironmentStub.Create,
+      TExternalResolverStub.Create, LBuild, LConfig);
+    try
+      LProgressPosition := -1;
+      LService.OnProgress :=
+        procedure(AMax, APosition: Integer)
+        begin
+          LProgressPosition := APosition;
+        end;
+      LService.Execute('Project.dproj');
+      Assert.AreEqual(1, LProgressPosition);
+      Assert.AreEqual(1, LBuild.CallCount);
+      Assert.AreEqual(0, LFiles.WriteCallCount);
+    finally
+      LService.Free;
+    end;
+  finally
+    TFile.Delete(LTempFile);
+  end;
+end;
+
+procedure TBuildReliabilityTests.InlineHintIsFixedAndBuildIsVerifiedAgain;
+var
+  LParser: TProjectParserSpy;
+  LAST: TASTParserStub;
+  LFiles: TFileServiceSpy;
+  LReports: TReportGeneratorStub;
+  LResolver: TExternalResolverStub;
+  LBuild: TSequencedBuildService;
+  LService: TProjectCleanerAppService;
+  LConfig: TToolConfig;
+  LTempFile: string;
+  LBaseline, LAfter, LVerified: TBuildMetrics;
+  LHint: TInlineHint;
+begin
+  LTempFile := TPath.GetTempFileName;
+  try
+    LParser := TProjectParserSpy.Create;
+    LParser.Units := [LTempFile];
+    LAST := TASTParserStub.Create;
+    LAST.SyntaxTree := TUnitSyntaxTreeStub.Create;
+    LFiles := TFileServiceSpy.Create;
+    LFiles.Content := 'unit TestUnit;' + sLineBreak + 'interface' + sLineBreak +
+      'uses Unused.Unit;' + sLineBreak + 'implementation' + sLineBreak +
+      'uses Needed.Unit;' + sLineBreak + 'end.';
+    LReports := TReportGeneratorStub.Create;
+    LResolver := TExternalResolverStub.Create;
+    LResolver.ResolveKnownUnits := True;
+    LBaseline := Default(TBuildMetrics);
+    LBaseline.Success := True;
+    LAfter := LBaseline;
+    LHint.HintType := 'H2443';
+    LHint.FilePath := LTempFile;
+    LHint.UnitNeeded := 'Needed.Unit';
+    LAfter.InlineHints := [LHint];
+    LVerified := LBaseline;
+    LBuild := TSequencedBuildService.Create;
+    LBuild.Results := [LBaseline, LAfter, LVerified];
+    LConfig := TToolConfig.Default;
+    LConfig.RemoveUnused := True;
+    LService := TProjectCleanerAppService.Create(LParser, LAST, LFiles, LReports,
+      TDelphiEnvironmentStub.Create, LResolver, LBuild, LConfig);
+    try
+      LService.Execute('Project.dproj');
+      Assert.AreEqual(3, LBuild.CallCount);
+      Assert.AreEqual(2, LFiles.WriteCallCount);
+      Assert.AreEqual(2, LFiles.BackupCallCount);
+      Assert.AreEqual(1, LFiles.CommitCallCount);
+      Assert.IsTrue(LFiles.Content.Contains('Needed.Unit'));
+      Assert.IsTrue(Pos('Needed.Unit', LFiles.Content) < Pos('implementation', LFiles.Content));
+    finally
+      LService.Free;
+    end;
+  finally
+    TFile.Delete(LTempFile);
   end;
 end;
 
